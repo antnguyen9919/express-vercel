e20cc27701022fbc074a43f942d1dc0663e495d3
An T. Nguyen <an.nguyen@tum.de>
2022-12-02 15:48:29 +0100

    second

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const express = require("express");
 // Initialize Express
 const app = express();
 
+// set the view engine to ejs
+app.set("view engine", "ejs");
 // Create GET request
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
-});
 
+app.get("/", function (req, res) {
+  res.render("pages/index", { name: "Hello world" });
+});
 // Initialize server
 app.listen(5000, () => {
   console.log("Running on port 5000.");
