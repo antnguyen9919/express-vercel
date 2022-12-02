683b7e22fbe0b02e3ecc7cce2af1379700a34782
An T. Nguyen <an.nguyen@tum.de>
2022-12-02 18:00:12 +0100

    lots

--- index.js
+++ index.js
@@ -1,17 +1,79 @@
 // Add Express
 const express = require("express");
-
+const Redis = require("ioredis");
 var path = require("path");
+const passport = require("passport");
+const flash = require("connect-flash");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
 // Initialize Express
 const app = express();
 
 // set the view engine to ejs
 app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "ejs");
-// Create GET request
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+// Connect flash
+app.use(flash());
+let client = new Redis(
+  "redis://default:a1e07c7df6b2437b9005d2a631d72b7d@eu2-driving-swine-30169.upstash.io:30169"
+);
 
 app.get("/", function (req, res) {
-  res.render("pages/index", { name: "Hello world" });
+  res.render("pages/home", { name: "Hello world" });
+});
+
+app.get("/users/login", function (req, res) {
+  res.render("pages/login");
+});
+app.get("/users/register", function (req, res) {
+  res.render("pages/register");
+});
+app.post("/users/register", function (req, res) {
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: "Please enter all fields" });
+  }
+
+  if (password != password2) {
+    errors.push({ msg: "Passwords do not match" });
+  }
+
+  if (password.length < 6) {
+    errors.push({ msg: "Password must be at least 6 characters" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      password2,
+    });
+  } else {
+    console.log("OK");
+  }
+  //   res.render("pages/register");
+});
+
+app.get("/dashboard", (req, res) => {
+  res.render("pages/dashboard", { user: { name: "An" } });
 });
 
 app.get("/api/users", function (req, res) {
