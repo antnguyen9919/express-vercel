b6aaecd97be6378659e356c3f9e52c98aa9334a1
An T. Nguyen <an.nguyen@tum.de>
2022-12-02 16:00:59 +0100

    addedAPI

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ app.set("view engine", "ejs");
 app.get("/", function (req, res) {
   res.render("pages/index", { name: "Hello world" });
 });
+
+app.get("/api/users", function (req, res) {
+  res.json({
+    users: [
+      { id: 1, name: "John Doe" },
+      { id: 2, name: "Jane Doe" },
+      { id: 3, name: "Mary Doe" },
+      { id: 4, name: "Peter Doe" },
+      { id: 5, name: "Mike Doe" },
+      { id: 6, name: "Jack Doe" },
+    ],
+  });
+});
+
 // Initialize server
 app.listen(5000, () => {
   console.log("Running on port 5000.");
