b6c2c1ae4d402ec910d2c636f8f575ed109b3b35
An T. Nguyen <an.nguyen@tum.de>
2022-12-03 09:24:16 +0100

    meh

--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ import axios from "axios";
 import "../styles/main.css";
 // require("highcharts/modules/exporting")(Highcharts);
 // require("highcharts/themes/dark-unica")(Highcharts);
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDxybiOd96OWzLLT04G2i2I9YzRe1Jn3_g",
+  authDomain: "express-js-management.firebaseapp.com",
+  projectId: "express-js-management",
+  storageBucket: "express-js-management.appspot.com",
+  messagingSenderId: "113062787866",
+  appId: "1:113062787866:web:a924d6c598596e4f5a8aa3",
+  measurementId: "G-S4L9RR1P3K",
+};
+const app = getApps().length < 1 ? initializeApp(firebaseConfig) : getApp();
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "production") {
+  const analytics = getAnalytics(app);
+  logEvent(analytics, "notification_received");
+}
 
 // Load the exporting module.
 import Exporting from "highcharts/modules/exporting";
